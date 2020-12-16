75f5b1f24932fbc81a8bd8662f4e58e03216d430
Filipp Gudis <fgudis@yahoo.com>
2020-12-15 20:22:07 -0800

    scoped styling using styled-jsx

--- components/User.js
+++ components/User.js
@@ -5,7 +5,19 @@ const user = (props) => (
     <div>
         <h1>{props.name}</h1>
         <p>Age: {props.age}</p>
+        <style jsx>{`
+            div {
+                border: 5px solid #eee;
+                box-shadow: 0 2px 3px #fff;
+                padding: 28 px;
+                text-align: center;
+            }
+        `}</style>
     </div>
 );
 
 export default user;
+
+// components in next.js are styled by using a package styled-jsx!!!
+// styling this way is scoped to our given component!!!!!!
+// more info in official docs
