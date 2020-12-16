e5ff7afad3f85d502d54e1578d7792fa0c1d2097
Filipp Gudis <fgudis@yahoo.com>
2020-12-15 20:58:28 -0800

    deployment details

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # NEXT.js
+
 simple introduction to next.js and it's basic functionality!!!
+npm run build - builds our project and makes it ready to be deployed
+VERY IMPORTANT - OUR NEXT.JS PROJECT NEEDS TO RUN ON A SERVER THAT
+SUPPORTS NODE.JS!!!!!
--- pages/_error.js
+++ pages/_error.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import Link from 'next/link';
-
+import React from "react";
+import Link from "next/link";
 
 const ErrorPage = () => (
-    
-        <div>
-            <h1>OOPs, wrong way</h1>
-            <p>Go back <Link href="/"><a>to 1st base</a></Link></p>
-        </div>
-    
+  <div>
+    <h1>OOPs, wrong way</h1>
+    <p>
+      Go back{" "}
+      <Link href="/">
+        <a>to 1st base</a>
+      </Link>
+    </p>
+  </div>
 );
 
 export default ErrorPage;
