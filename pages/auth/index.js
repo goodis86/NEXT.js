2b0e63ae0317e9865a0495c3eb64370ea49ac68f
Filipp Gudis <fgudis@yahoo.com>
2020-12-15 20:52:12 -0800

    getInitialProps lyfecycle hook in action!

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -3,17 +3,29 @@ import Link from 'next/link';
 
 import User from '../../components/User';
 
-const authIndexPage = () => (
+const authIndexPage = (props) => (
     
         <div>
-            <h1>The Auth Page</h1>  
+            <h1>The Auth Page - {props.appName}</h1>  
             <p>Go to <Link href="/"><a>Main</a></Link></p>        
             <User name="Phil" age={34} />
         </div>
     
 );
 
+authIndexPage.getInitialProps = (context) => {      // example of our getInitialProps working with functional compoenent as welll!!!!!
+    const promise = new Promise((resolse, reject) => {
+        setTimeout(() => {
+            resolse({appName: "super man Phil Authorization"});
+        }, 1000);
+    });
+    return promise;
+}
+
 export default authIndexPage;
 
 // use of a regular component is the same as in react
 // only special treatment gets folder *pages* that works as a router for us and does lazy loading as well!!!!!!!!!!!!
+
+// getinitial props - very usefull lifecycle that works in class based and functional components!!!
+//helps initialize our props before the page is actually loaded...
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,37 @@
-import React from 'react';
-import Link from 'next/link';
-import Router from 'next/router';
+import React, { Component } from "react";
+import Link from "next/link";
+import Router from "next/router";
 
-const indexPage = () => (
-    
-        <div>
-            <h1>The main Page</h1>
-            <p>Go to <Link href="/auth"><a>Auth</a></Link></p>
-            <button onClick = {() => Router.push('/auth')}>Go to Auth as well</button> 
-        </div>
-    
-);
+class IndexPage extends Component {
 
-export default indexPage;
+    static getInitialProps(context) {    // very important lifecycle hook that prepopulates our page before rendering in to the client's screen!!!!!!!
+        console.log(context);               // it waits for our promises to resolve and then prepopulates our props!
+        const promise = new Promise((resolse, reject) => {
+            setTimeout(() => {
+                resolse({appName: "super man Phil app"});
+            }, 1000);
+        });
+        return promise;
+    }
+
+  render() {
+    return (
+      <div>
+        <h1>The main Page {this.props.appName}</h1>
+        <p>
+          Go to{" "}
+          <Link href="/auth">
+            <a>Auth</a>
+          </Link>
+        </p>
+        <button onClick={() => Router.push("/auth")}>Go to Auth as well</button>
+      </div>
+    );
+  }
+}
+export default IndexPage;
 
 //next has it's own link component as well, has to be wrapped in <a>
-
 
 // the main idea behind next.js is file and folder structure that lets us navigate and avoid using routes at all,
 // it does it for us!!!!
